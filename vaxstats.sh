8a080a854f1a8071e60987a02c7766ad361c61b6
sykul <51741333+sykul@users.noreply.github.com>
2021-05-01 20:39:24 +0800

    Update vaxstats.sh
    Changed data source to gov.uk data (can choose fields and is UK-specific so smaller download).
    Now shows fully vaccinated, cumulative first dose, and daily increases for both.
    
    To-do:
    Maybe move data extraction to within the if loop to save computation.

--- vaxstats.sh
+++ vaxstats.sh
@@ -1,50 +1,80 @@
-datafile=$(ls -l --time-style=+"%Y-%m-%d" /tmp/ | grep "$(date --iso-8601)" | grep "data?areaType=overview&metric=cumPeopleVaccinatedFirstDoseByPublishDate&format=csv" | cut -d " " -f "15")
+datafile=$(ls -l --time-style=+"%Y-%m-%d" /tmp/ | grep "$(date --iso-8601)" | grep "data?areaType=overview&metric=cumPeopleVaccinatedCompleteByPublishDate&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedCompleteByPublishDate&format=csv" | cut -d " " -f "15")
 
 today="$(date +"%m/%d")"
 yesterday="$(date +"%m/%d" -d "1 day ago")"
 daybeforeyesterday="$(date +"%m/%d" -d "2 days ago")"
 threedaysago="$(date +"%m/%d" -d "3 days ago")"
 
+# Checks whether today's data file exists in your /tmp directory. If not, downloads it to /tmp.
 if [ -z "$datafile" ]
 then
-        notify-send "Downloading vaccination data." "Please hold."
-        wget -q -P /tmp/ "https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumPeopleVaccinatedCompleteByPublishDate&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedCompleteByPublishDate&format=csv"
-        datafile=$(ls -l --time-style=+"%Y-%m-%d" /tmp/ | grep "$(date --iso-8601)" | grep "data?areaType=overview&metric=cumPeopleVaccinatedFirstDoseByPublishDate&format=csv" | cut -d " " -f "15")
+	notify-send "Downloading vaccination data." "Please hold."
+	wget -q -P /tmp/ "https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=cumPeopleVaccinatedCompleteByPublishDate&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedCompleteByPublishDate&format=csv"
+	datafile=$(ls -l --time-style=+"%Y-%m-%d" /tmp/ | grep "$(date --iso-8601)" | grep "data?areaType=overview&metric=cumPeopleVaccinatedCompleteByPublishDate&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedCompleteByPublishDate&format=csv" | cut -d " " -f "15")
 fi
 
-todaydata=$(grep -e "$(date --iso-8601)" "/tmp/$datafile" | cut -d "," -f "5")
-yesterdaydata=$(grep -e "$(date --iso-8601 -d "1 day ago")" "/tmp/$datafile" | cut -d "," -f "5")
-daybeforeyesterdaydata=$(grep -e "$(date --iso-8601 -d "2 days ago")" "/tmp/$datafile" | cut -d "," -f "5")
-threedaysagodata=$(grep -e "$(date --iso-8601 -d "3 days ago")" "/tmp/$datafile" | cut -d "," -f "5")
+# Sets variables extracted from the data file. I might move these to the if loop later.
+todaycumcomplete=$(grep -e "$(date --iso-8601)" "/tmp/$datafile" | cut -d "," -f "5")
+yesterdaycumcomplete=$(grep -e "$(date --iso-8601 -d "1 day ago")" "/tmp/$datafile" | cut -d "," -f "5")
+daybeforeyesterdaycumcomplete=$(grep -e "$(date --iso-8601 -d "2 days ago")" "/tmp/$datafile" | cut -d "," -f "5")
+threedaysagocumcomplete=$(grep -e "$(date --iso-8601 -d "3 days ago")" "/tmp/$datafile" | cut -d "," -f "5")
 
-if [ -n "$todaydata" ] 
+todaycumfirstdose=$(grep -e "$(date --iso-8601)" "/tmp/$datafile" | cut -d "," -f "6")
+yesterdaycumfirstdose=$(grep -e "$(date --iso-8601 -d "1 day ago")" "/tmp/$datafile" | cut -d "," -f "6")
+daybeforeyesterdaycumfirstdose=$(grep -e "$(date --iso-8601 -d "2 days ago")" "/tmp/$datafile" | cut -d "," -f "6")
+threedaysagocumfirstdose=$(grep -e "$(date --iso-8601 -d "3 days ago")" "/tmp/$datafile" | cut -d "," -f "6")
+
+todaynewcomplete=$(grep -e "$(date --iso-8601)" "/tmp/$datafile" | cut -d "," -f "7")
+yesterdaynewcomplete=$(grep -e "$(date --iso-8601 -d "1 day ago")" "/tmp/$datafile" | cut -d "," -f "7")
+daybeforeyesterdaynewcomplete=$(grep -e "$(date --iso-8601 -d "2 days ago")" "/tmp/$datafile" | cut -d "," -f "7")
+threedaysagonewcomplete=$(grep -e "$(date --iso-8601 -d "3 days ago")" "/tmp/$datafile" | cut -d "," -f "7")
+
+todaynewfirstdose=$(grep -e "$(date --iso-8601)" "/tmp/$datafile" | cut -d "," -f "8")
+yesterdaynewfirstdose=$(grep -e "$(date --iso-8601 -d "1 day ago")" "/tmp/$datafile" | cut -d "," -f "8")
+daybeforeyesterdaynewfirstdose=$(grep -e "$(date --iso-8601 -d "2 days ago")" "/tmp/$datafile" | cut -d "," -f "8")
+threedaysagonewfirstdose=$(grep -e "$(date --iso-8601 -d "3 days ago")" "/tmp/$datafile" | cut -d "," -f "8")
+
+# Calculates and outputs percentages. Attempts to get the most recent data available.
+if [ -n "$todaycumcomplete" ] 
 then
-        todaypercent=$(echo "$todaydata/68171922*100" | bc -l | cut -c -5)
-        echo "$todaypercent% as of $today"
-        notify-send "Vaccinated:" "$todaypercent% as of $today"
-        break
+	todaypercentcumcomplete=$(echo "$todaycumcomplete/68171922*100" | bc -l | cut -c -5)
+	todaypercentcumfirstdose=$(echo "$todaycumfirstdose/68171922*100" | bc -l | cut -c -5)
+	todaypercentnewcomplete=$(echo "$todaynewcomplete/68171922*100" | bc -l | cut -c -5)
+	todaypercentnewfirstdose=$(echo "$todaynewfirstdose/68171922*100" | bc -l | cut -c -5)
+        echo "$todaypercentcumfirstdose% with at least one dose, $todaypercentcumcomplete% fully vaccinated. That's an increase of $todaypercentnewfirstdose% first doses and $todaypercentnewcomplete% second doses since the day before. Data last updated $today"
+	notify-send "$todaypercentcumfirstdose% with at least one dose, $todaypercentcumcomplete% fully vaccinated. That's an increase of $todaypercentnewfirstdose% first doses and $todaypercentnewcomplete% second doses since the day before. Data last updated $today"
+	break
 else
-        if [ -n "$yesterdaydata" ]
-        then
-                yesterdaypercent=$(echo "$yesterdaydata/68171922*100" | bc -l | cut -c -5)
-                echo "$yesterdaypercent% as of $yesterday"
-                notify-send "Vaccinated" "$yesterdaypercent% as of $yesterday"
-                break
-        else
-                if [ -n "$daybeforeyesterdaydata" ]
-                then
-                        daybeforeyesterdaypercent=$(echo "$daybeforeyesterdaydata/68171922*100" | bc -l | cut -c -5)
-                        echo "$daybeforeyesterdaypercent% as of $daybeforeyesterday"
-                        notify-send "Vaccinated" "$daybeforeyesterdaypercent% as of $daybeforeyesterday"
-                        break
-                else
-                        if [ -n "$threedaysagodata" ]
-                        then
-                                threedaysagopercent=$(echo "$threedaysagodata/68171922*100" | bc -l | cut -c -5)
-                                echo "$threedaysagopercent% as of $threedaysago"
-                                notify-send "$threedaysagopercent% as of $threedaysago"
-                                break
-                        fi
-                fi
-        fi
+        if [ -n "$yesterdaycumcomplete" ]
+	then
+		yesterdaypercentcumcomplete=$(echo "$yesterdaycumcomplete/68171922*100" | bc -l | cut -c -5)
+		yesterdaypercentcumfirstdose=$(echo "$yesterdaycumfirstdose/68171922*100" | bc -l | cut -c -5)
+		yesterdaypercentnewcomplete=$(echo "$yesterdaynewcomplete/68171922*100" | bc -l | cut -c -5)
+		yesterdaypercentnewfirstdose=$(echo "$yesterdaynewfirstdose/68171922*100" | bc -l | cut -c -5)
+        	echo "$yesterdaypercentcumfirstdose% with at least one dose, $yesterdaypercentcumcomplete% fully vaccinated. That's an increase of $yesterdaypercentnewfirstdose% first doses and $yesterdaypercentnewcomplete% second doses since the day before. Data last updated $yesterday"
+		notify-send "$yesterdaypercentcumfirstdose% with at least one dose, $yesterdaypercentcumcomplete% fully vaccinated. That's an increase of $yesterdaypercentnewfirstdose% first doses and $yesterdaypercentnewcomplete% second doses since the day before. Data last updated $yesterday"
+		break
+	else
+		if [ -n "$daybeforeyesterdaycumcomplete" ]
+		then
+			daybeforeyesterdaypercentcumcomplete=$(echo "$daybeforeyesterdaycumcomplete/68171922*100" | bc -l | cut -c -5)
+			daybeforeyesterdaypercentcumfirstdose=$(echo "$daybeforeyesterdaycumfirstdose/68171922*100" | bc -l | cut -c -5)
+			daybeforeyesterdaypercentnewcomplete=$(echo "$daybeforeyesterdaynewcomplete/68171922*100" | bc -l | cut -c -5)
+			daybeforeyesterdaypercentnewfirstdose=$(echo "$daybeforeyesterdaynewfirstdose/68171922*100" | bc -l | cut -c -5)
+        		echo "$daybeforeyesterdaypercentcumfirstdose% with at least one dose, $daybeforeyesterdaypercentcumcomplete% fully vaccinated. That's an increase of $daybeforeyesterdaypercentnewfirstdose% first doses and $daybeforeyesterdaypercentnewcomplete% second doses since the day before. Data last updated $daybeforeyesterday"
+			notify-send "$daybeforeyesterdaypercentcumfirstdose% with at least one dose, $daybeforeyesterdaypercentcumcomplete% fully vaccinated. That's an increase of $daybeforeyesterdaypercentnewfirstdose% first doses and $daybeforeyesterdaypercentnewcomplete% second doses since the day before. Data last updated $daybeforeyesterday"
+			break
+		else
+			if [ -n "$threedaysagocumcomplete" ]
+			then
+				threedaysagopercentcumcomplete=$(echo "$threedaysagocumcomplete/68171922*100" | bc -l | cut -c -5)
+				threedaysagopercentcumfirstdose=$(echo "$threedaysagocumfirstdose/68171922*100" | bc -l | cut -c -5)
+				threedaysagopercentnewcomplete=$(echo "$threedaysagonewcomplete/68171922*100" | bc -l | cut -c -5)
+				threedaysagopercentnewfirstdose=$(echo "$threedaysagonewfirstdose/68171922*100" | bc -l | cut -c -5)
+        			echo "$threedaysagopercentcumfirstdose% with at least one dose, $threedaysagopercentcumcomplete% fully vaccinated. That's an increase of $threedaysagopercentnewfirstdose% first doses and $threedaysagopercentnewcomplete% second doses since the day before. Data last updated $threedaysago"
+				notify-send "$threedaysagopercentcumfirstdose% with at least one dose, $threedaysagopercentcumcomplete% fully vaccinated. That's an increase of $threedaysagopercentnewfirstdose% first doses and $threedaysagopercentnewcomplete% second doses since the day before. Data last updated $threedaysago"
+				break
+			fi
+		fi
+	fi
 fi
